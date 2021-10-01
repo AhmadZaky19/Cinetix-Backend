6797f9cf29280bfe6d6993db074bbf83faf5cbf6
Ahmad Zaky <a.zaky32@gmail.com>
2021-10-01 14:30:32 +0700

    add deleteFile helper

---
+++ src/helpers/uploads/deleteFile.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+// const helperWrapper = require("../wrapper");
+
+const deleteFile = (filePath) => {
+  // eslint-disable-next-line no-console
+  console.log("PROSES DELETE", filePath);
+  // fs.existSync
+  if (fs.existsSync(filePath)) {
+    // fs.unlinkSync
+    fs.unlinkSync(filePath, (error) => {
+      if (error) {
+        return error.message("delete unsuccessful");
+      }
+    });
+  }
+};
+
+module.exports = deleteFile;
