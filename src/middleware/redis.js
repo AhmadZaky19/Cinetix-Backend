1d3bc9a9da01ad0a1de1220d3286ab8a743c3581
Ahmad Zaky <a.zaky32@gmail.com>
2021-10-02 10:42:57 +0700

    add redis for schedule modules

--- src/middleware/redis.js
+++ src/middleware/redis.js
@@ -50,4 +50,52 @@ module.exports = {
       next();
     });
   },
+  getScheduleByIdRedis: (req, res, next) => {
+    const { id } = req.params;
+    redis.get(`getSchedule:${id}`, (error, result) => {
+      if (!error && result !== null) {
+        // eslint-disable-next-line no-console
+        console.log("Data ada di dalam redis");
+        const newResult = JSON.parse(result);
+        return helperWrapper.response(
+          res,
+          200,
+          "Success get data by id",
+          newResult
+        );
+      }
+      // eslint-disable-next-line no-console
+      console.log("Data tidak ada di dalam redis");
+      next();
+    });
+  },
+  getScheduleRedis: (req, res, next) => {
+    redis.get(`getSchedule:${JSON.stringify(req.query)}`, (error, result) => {
+      if (!error && result !== null) {
+        // eslint-disable-next-line no-console
+        console.log("Data ada di dalam redis");
+        const newResult = JSON.parse(result);
+        return helperWrapper.response(
+          res,
+          200,
+          "Success get data",
+          newResult.result,
+          newResult.pageInfo
+        );
+      }
+      // eslint-disable-next-line no-console
+      console.log("Data tidak ada di dalam redis");
+      next();
+    });
+  },
+  clearScheduleRedis: (req, res, next) => {
+    redis.keys("getSchedule:*", (error, result) => {
+      if (result.length > 0) {
+        result.forEach((item) => {
+          redis.del(item);
+        });
+      }
+      next();
+    });
+  },
 };
